b47476cb072901898d2b7f5a8c93bf2cec5fc81f
Joseph Ochego <josephochego@gmail.com>
2025-11-08 14:43:17 +0300

    added PropertyBadgeProp to the interface

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -16,3 +16,12 @@ export interface ButtonProps {
   variant?: "primary" | "secondary" | "outline"; // visual style variant
   disabled?: boolean; // disabled state
 }
+
+export interface PropertyBadgeProps {
+  // status: string; // this prop would be open for any word outside of the
+  // list below
+  status: "New" | "Featured" | "Available" | "Sold!"; // These are the only
+  // strings allowed for the status of the property listing
+  color: "badge-blue" | "badge-green" | "badge-red" | "badge-gray"; // colour
+  // coded statuses
+}
